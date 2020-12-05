f1103dfa401dafa58a29823017997996e34e5e5b
Alexey Lebedeff <binarin@binarin.ru>
2020-12-05 21:01:10 +0100

    Day 01

--- Main.hs
+++ Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import qualified Data.Set as S
+
+day01 = do
+  nums :: [Int] <- fmap read . lines <$> readFile "data/01.txt"
+  let lookup = S.fromList nums
+      solution = filter (flip S.member lookup . (2020-)) nums
+  print $ head solution * (solution !! 1)
+
+  let solution2 = [ n1 * n2 * (2020 - n1 - n2)
+                  | n1 <- nums
+                  , n2 <- nums
+                  , S.member (2020 - n1 - n2) lookup
+                  ]
+
+  print solution2
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  day01
